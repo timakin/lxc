c179cd617fd9bf391447badf441178508e65deb2
Zachary Patten <zachary@jovelabs.com>
2013-04-13 22:35:22 -0700

    update the container spec

--- spec/lxc/container_spec.rb
+++ spec/lxc/container_spec.rb
@@ -4,19 +4,7 @@ describe LXC::Container do
 
   TEST_CONTAINER_NAME = "test-container"
 
-  subject {
-
-    ssh_connection = ::ZTK::SSH.new(
-      :host_name => "127.0.0.1",
-      :user => ENV['USER'],
-      :keys => "#{ENV['HOME']}/.ssh/id_rsa"
-    ).ssh
-
-    @lxc = LXC.new
-    @lxc.use_ssh = ssh_connection
-
-    LXC::Container.new(@lxc, TEST_CONTAINER_NAME)
-  }
+  subject { LXC::Container.new(LXC.new, TEST_CONTAINER_NAME) }
 
   describe "class" do
 
@@ -27,11 +15,9 @@ describe LXC::Container do
     describe "attributes" do
 
       describe "#name" do
-
         it "should be readable and match what was passed to the initializer" do
           subject.name.should == TEST_CONTAINER_NAME
         end
-
       end
 
    end
@@ -59,11 +45,6 @@ describe LXC::Container do
     end
 
     describe "#wait" do
-      it "should timeout while waiting for a non-existant remote container to start" do
-        subject.wait([:running], 1).should == false
-        %x(pkill -f lxc-wait)
-      end
-
       it "should be successfully when waiting to stop a non-existant remote container" do
         subject.wait([:stopped], 120).should == true
       end
