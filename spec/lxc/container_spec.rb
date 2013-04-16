98a72209d4995adf5fff7294f8041955b86716ab
Zachary Patten <zachary@jovelabs.com>
2013-04-16 14:18:06 -0700

    add more container specs

--- spec/lxc/container_spec.rb
+++ spec/lxc/container_spec.rb
@@ -29,8 +29,56 @@ describe LXC::Container do
     LXC_VERSIONS.each do |lxc_version|
       context "LXC Target Version #{lxc_version}" do
 
+        describe "#stopped?" do
+          it "should return true for an un-created container" do
+            subject.stopped?.should == true
+          end
+        end
+
+        describe "#starting?" do
+          it "should return false for an un-created container" do
+            subject.starting?.should == false
+          end
+        end
+
+        describe "#running?" do
+          it "should return false for an un-created container" do
+            subject.running?.should == false
+          end
+        end
+
+        describe "#stopping?" do
+          it "should return false for an un-created container" do
+            subject.stopping?.should == false
+          end
+        end
+
+        describe "#aborting?" do
+          it "should return false for an un-created container" do
+            subject.aborting?.should == false
+          end
+        end
+
+        describe "#freezing?" do
+          it "should return false for an un-created container" do
+            subject.freezing?.should == false
+          end
+        end
+
+        describe "#frozen?" do
+          it "should return false for an un-created container" do
+            subject.frozen?.should == false
+          end
+        end
+
+        describe "#thawed?" do
+          it "should return false for an un-created container" do
+            subject.thawed?.should == false
+          end
+        end
+
         describe "#exists?" do
-          it "should return false for an un-created remote container" do
+          it "should return false for an un-created container" do
             subject.stub(:exec) { lxc_fixture(lxc_version, "lxc-ls-wo-containers.out") }
 
             subject.exists?.should == false
@@ -38,7 +86,7 @@ describe LXC::Container do
         end
 
         describe "#pid" do
-          it "should return -1 for an un-created remote container" do
+          it "should return -1 for an un-created container" do
             subject.stub(:exec) { lxc_fixture(lxc_version, "lxc-info-pid-stopped.out") }
 
             subject.pid.should == -1
@@ -46,7 +94,7 @@ describe LXC::Container do
         end
 
         describe "#state" do
-          it "should return stopped for an un-created remote container" do
+          it "should return stopped for an un-created container" do
             subject.stub(:exec) { lxc_fixture(lxc_version, "lxc-info-state-stopped.out") }
 
             subject.state.should == :stopped
@@ -54,10 +102,17 @@ describe LXC::Container do
         end
 
         describe "#wait" do
-          it "should be successfully when waiting to stop a non-existant remote container" do
+          it "should be successfully when waiting to stop a non-existant container" do
             subject.stub(:exec) { lxc_fixture(lxc_version, "lxc-wait.out") }
 
             subject.wait([:stopped], 120).should == true
+          end
+        end
+
+        describe "#inspect" do
+          it "should return an information string about our class instance" do
+            subject.inspect.should be_kind_of(String)
+            subject.inspect.length.should be > 0
           end
         end
 
